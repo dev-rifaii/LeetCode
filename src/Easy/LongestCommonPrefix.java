d14342bbaec41929a90f8a92e7a1e89c7b2c8032
Mohammad <moe.rifaii77@gmail.com>
2023-03-29 20:30:36 +0300

    Fix Longest Common Prefix

--- src/Easy/LongestCommonPrefix.java
+++ src/Easy/LongestCommonPrefix.java
@@ -3,29 +3,22 @@ package Easy;
 //https://leetcode.com/problems/longest-common-prefix/
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        String longestPrefix = "";
-        int occurrences = 0;
+
         if (strs.length == 0) {
-            return longestPrefix;
+            return "";
         }
 
-        for (int i = 0; i < strs.length; i++) {
-            for (int j = 0; j < strs.length - 1 && j != i; j++) {
-                String currentLongest = "";
-                int currentOccurrences = 0;
-                for (int k = 0; k < Math.min(strs[i].length(), strs[j].length()); k++) {
-                    if (strs[i].charAt(k) == strs[j].charAt(k)) {
-                        currentLongest += strs[i].charAt(k);
-                        currentOccurrences++;
-                    } else {
-                        break;
-                    }
-                }
-                if (currentOccurrences > occurrences) {
-                    longestPrefix = currentLongest;
-                }
+        String currentLongest = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < currentLongest.length()) {
+                currentLongest = currentLongest.substring(0, strs[i].length());
             }
+            for (int j = 0; j < currentLongest.length(); j++)
+                if (currentLongest.charAt(j) != strs[i].charAt(j)) {
+                    currentLongest = currentLongest.substring(0, j);
+                }
         }
-        return longestPrefix;
+
+        return currentLongest;
     }
 }
