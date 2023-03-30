622804b39b986a3a083365d834c9edd33221cc8f
Mohammad <moe.rifaii77@gmail.com>
2023-03-30 22:53:07 +0300

    Solve Valid Palindrome

---
+++ src/Easy/ValidPalindrome.java
@@ -0,0 +1,17 @@
+package Easy;
+
+//https://leetcode.com/problems/valid-palindrome/
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "")
+                .toLowerCase();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != s.charAt((s.length() - 1) - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
