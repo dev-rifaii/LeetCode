9d1c7eb55d8e652f5c52692efa3a9ff06bf3799e
Mohammad <moe.rifaii77@gmail.com>
2023-04-07 23:17:17 +0300

    Solve Can Place Flowers

---
+++ src/Easy/CanPlaceFlowers.java
@@ -0,0 +1,16 @@
+package Easy;
+
+//https://leetcode.com/problems/can-place-flowers/
+public class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0
+                    && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                n--;
+                i += 1;
+            }
+        }
+        return n < 1;
+    }
+}
