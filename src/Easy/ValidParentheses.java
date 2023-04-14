f6120df3579011dffa435e87a7f6f3908d824267
Mohammad <moe.rifaii77@gmail.com>
2023-04-14 23:42:52 +0300

    Improve Valid Parentheses

--- src/Easy/ValidParentheses.java
+++ src/Easy/ValidParentheses.java
@@ -4,25 +4,32 @@ import java.util.Stack;
 
 //Link: https://leetcode.com/problems/valid-parentheses/
 public class ValidParentheses {
-    public static boolean isValid(String s) {
+
+    public boolean isValid(String s) {
         if (s.length() % 2 != 0) {
             return false;
         }
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                stack.push(s.charAt(i));
-            } else if (s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if (s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else {
-                return false;
+            switch (s.charAt(i)) {
+                case '(', '[', '{' -> stack.push(s.charAt(i));
+                case ')' -> {
+                    if (!stack.isEmpty() && stack.peek() == '(') stack.pop();
+                    else return false;
+                }
+                case ']' -> {
+                    if (!stack.isEmpty() && stack.peek() == '[') stack.pop();
+                    else return false;
+                }
+                case '}' -> {
+                    if (!stack.isEmpty() && stack.peek().equals('{')) stack.pop();
+                    else return false;
+                }
+                default -> {
+                    return false;
+                }
             }
         }
-        return stack.isEmpty() ? true : false;
+        return stack.isEmpty();
     }
-
 }
